5e710cf1d0d3013bdc9925b6e66a28899f2c692a
fabio.lopes <Abacate#23>
2021-08-29 17:21:17 -0300

    Inclusão de função para leitura de arquivos CSV;

---
+++ src/test/java/datadriven/UserDD.java
@@ -0,0 +1,49 @@
+// 1 - pacote
+package datadriven;
+
+// 2 - bibliotecas
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.Data;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class UserDD {
+    // 3.1 - atributos
+    String uri = "https://petstore.swagger.io/v2/user";
+    Data data; // obj que representa classe utils.Data
+
+    // 3.2 - Métodos e Funções
+
+    @BeforeMethod
+    public void  setup(){
+        data = new Data();
+    }
+
+    @Test
+    public void incluirUsusario() throws IOException {
+        String jsonBody = data.lerJson("db/user1.json");
+
+        String userId =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .body(jsonBody)
+                .when()
+                        .post(uri)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("code",is (200))
+                        .body("type",is("unknown"))
+                .extract()
+                        .path("message")
+                ;
+        System.out.println("O userId e: " + userId);
+    }
+}
--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,7 +1,8 @@
-//1- Pacote
+// https://petstore.swagger.io/
+
+// 1 - Pacote
 package petstore;
 
-//2 - Biblioteca
 
 import org.testng.annotations.Test;
 
@@ -13,62 +14,60 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
 
-//3 - Classe
-
 public class Pet {
-    //3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
-    //3.2 - Métodos e Funções
+    String uri = "https://petstore.swagger.io/v2/pet"; //
+
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    // Incluir - Create - Post
-    @Test // Identifica o método ou função como um teste para o TestNG
+    @Test(priority = 1)
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
-        // Sintaxe Guerkin
-        // Dado - Quando - Então
-        // Given - When - Then
-
-        given() //Dado
-                .contentType("application/json") //comum em API REST - antigas era "text/xml"
+        given() // Dado
+                .contentType("application/json")
                 .log().all()
                 .body(jsonBody)
-        .when() //Quando
+                .when()  // Quando
                 .post(uri)
-        .then() //Então
+                .then()  // Então
                 .log().all()
-                .statusCode(200)
                 .statusCode(200)
                 .body("name", is("Dante"))
                 .body("status", is("available"))
-                .body("category.name", is("Dog"))
-                .body("tags.name", contains("STA"))
+                .body("category.name", is("Dog"))//AX2345LORT
+                .body("tags.name", contains("data"))
         ;
-    }
-    @Test
-    public void consultaPet(){
-        String petId = "151517111987";
 
-        given()
-                .contentType("applcation/json")
-                .log().all()
-                .when()
-                .get(uri + "/" + petId)
-        .then()
-                .log().all()
-                .statusCode(200)
-                .body("name", is("Dante"))
-                .body("category.name", is("Dog"))
-                .body("status", is("available"))
 
-     ;
-        System.out.println("Requisição de dados: ");
     }
-    @Test
+
+    @Test(priority=2)
+    public void consultarPet(){
+        String petId = "21082021";
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .when()
+                        .get(uri + "/" + petId)
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Dante"))
+                        .body("category.name", is("Dog"))
+                        .body("status",is("available"))
+                        .extract()
+                        .path("category.name")
+                ;
+        System.out.println("O token é " + token);
+
+    }
+
+    @Test(priority=3)
     public void alterarPet() throws IOException {
         String jsonBody = lerJson("db/pet2.json");
 
@@ -76,16 +75,50 @@ public class Pet {
                 .contentType("application/json")
                 .log().all()
                 .body(jsonBody)
-        .when()
+                .when()
                 .put(uri)
-        .then()
+                .then()
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Dante"))
-                .body("status",is("Unavailable"))
-
-
-;
+                .body("status",is("sold"))
+        ;
     }
+
+    @Test (priority = 4)
+    public void excluirPet(){
+        String petId = "21082021";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .when()
+                .delete(uri + "/" + petId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+
+        ;
+    }
+
+    @Test (priority = 5)
+    public void consultaPetPorStatus(){
+        String status = "available";
+
+        given() //Dado que
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/findByStatus?status=" + status)
+        .then()
+                .log().all()
+                .statusCode(200)
+                //.body("name", contains ("Dante"))
+        ;
+    }
+
 
 }
